86db5e636f33ad10dca6ba7c36aa1f31c72dd605
Ashish <ashishgiri2705@gmail.com>
2025-05-23 14:39:07 +0530

    second version of data saved

--- mycode.py
+++ mycode.py
@@ -5,6 +5,8 @@ data ={"Name":["Alice","Bob","Charlie"],
        'Age':[25,30,35],
        'City':['Newyork','Los Angeles','Chicago']}
 df=pd.DataFrame(data)
+new_row_loc={'Name':'gf', 'Age':20,'City':'London'}
+df.loc[len(df.index)]=new_row_loc
 data_dir='data'
 os.makedirs(data_dir,exist_ok=True)
 file_path=os.path.join(data_dir,'sample_data.csv')  
